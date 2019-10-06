7b9708bdadef5119d5c872bd2e2b599df7045a56
mynet4u <45580862+mynet4u@users.noreply.github.com>
2019-10-07 01:43:27 +0900

    Create solution.cpp

---
+++ src/linkedlist/solution.cpp
@@ -0,0 +1,51 @@
+typedef struct _node {
+	int x;
+	int y;
+	int value;
+} NODE;
+
+void Init(void)
+{
+}
+
+NODE * Find(int x, int y)
+{
+}
+
+void Remove(NODE * node)
+{
+}
+
+NODE * Insert(NODE * node, int x, int y)
+{
+	return 0;
+}
+
+NODE * Push(int x, int y)
+{
+	return 0;
+}
+
+void Print(int* N, NODE list[])
+{
+}
+
+void ReversalPrint(int* N, NODE list[])
+{
+}
+
+void PrintNode(NODE* node, int* x, int* y)
+{
+}
+
+void AddSList(int value)
+{
+}
+
+void DelSList(int value)
+{
+}
+
+void PrintSList(int* N, NODE list[])
+{
+}
