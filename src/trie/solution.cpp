27f8a86ca0b7bdcf2e36aabcbf69e8be64c67c15
mynet4u <45580862+mynet4u@users.noreply.github.com>
2019-10-07 01:39:49 +0900

    Create solution.cpp

---
+++ src/trie/solution.cpp
@@ -0,0 +1,18 @@
+#define MAX_RESULT_SIZE 500
+#define MAX_STRING_SIZE 20
+#define MAX_NODE 10000
+#define nullptr 0L
+
+void init()
+{
+}
+void add(char str[])
+{
+}
+int find(char str[])
+{
+	return 0;
+}
+void list(char res[][MAX_STRING_SIZE], int& resIdx)
+{
+}
